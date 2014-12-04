59cbb60e45f0f7545527c59ff4be21ead44de005
Nick Rawe <rawebone@gmail.com>
2014-12-04 18:44:46 +0000

    Removed Dispatcher and Environment as these are now consolidated into the API object

--- library/Api.php
+++ library/Api.php
@@ -22,150 +22,115 @@ use Wilson\Injection\ProviderInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-/**
- * Api represents
- */
 class Api
 {
 	/**
-	 * @var Dispatcher
+	 * @var string
 	 */
-	protected $dispatcher;
+	public $cachePath;
+
+	/**
+	 * @var callable
+	 */
+	public $error;
 
 	/**
 	 * @var Injector
 	 */
-	protected $injector;
+	public $injector;
 
 	/**
-	 * @var <string, object>[]
+	 * @var callable
 	 */
-	protected $resources = array();
+	public $notFound;
 
 	/**
-	 * @param Dispatcher $dispatcher
-	 * @param Injector $injector
+	 * @var object[]
 	 */
-	public function __construct(Dispatcher $dispatcher,	Injector $injector)
+	public $resources = array();
+
+	public function __construct()
 	{
-		$this->dispatcher = $dispatcher;
-		$this->injector   = $injector;
+		$this->injector = new Injector();
+
+		$this->error = function ()
+		{
+
+		};
+
+		$this->notFound = function ()
+		{
+
+		};
 	}
 
-	/**
-	 * @param object $resource
-	 * @return $this
-	 * @throws \InvalidArgumentException
-	 */
-	public function attach($resource)
+	public function run()
 	{
-		if (!is_object($resource)) {
-			throw new \InvalidArgumentException(sprintf(
-				"\$resource should be an object, %s given",
-				gettype($resource)
-			));
-		}
+		$req  = Request::createFromGlobals();
+		$resp = new Response();
 
-		$this->resources[get_class($resource)] = $resource;
-		return $this;
-	}
-
-	/**
-	 * @param ProviderInterface $provider
-	 * @return $this
-	 */
-	public function services(ProviderInterface $provider)
-	{
-		$provider->register($this->injector);
-		return $this;
-	}
-
-	/**
-	 * The handler to be called when an error occurs.
-	 *
-	 * @param callable $callable
-	 * @return $this
-	 */
-	public function error($callable)
-	{
-		if (!is_callable($callable)) {
-			throw new \InvalidArgumentException("Argument should be callable");
-		}
-
-		$this->dispatcher->error($callable);
-		return $this;
-	}
-
-	/**
-	 * The handler to be called when a route does not match.
-	 *
-	 * @param callable $callable
-	 * @return $this
-	 */
-	public function notFound($callable)
-	{
-		if (!is_callable($callable)) {
-			throw new \InvalidArgumentException("Argument should be callable");
-		}
-
-		$this->dispatcher->notFound($callable);
-		return $this;
-	}
-
-	/**
-	 * @param Request $req
-	 * @param Response $resp
-	 */
-	public function run(Request $req = null, Response $resp = null)
-	{
-		$req = $req ?: Request::createFromGlobals();
-		$resp = $resp ?: new Response();
+		$resp->prepare($req);
 
 		$this->injector->instance("req", $req);
 		$this->injector->instance("resp", $resp);
 
-		$this->dispatcher->tryDispatch($this->resources, $req, $resp);
+		$this->tryDispatch($req, $resp);
 
 		$resp->send();
 	}
 
 	/**
-	 * Creates a new instance of the API.
-	 *
-	 * @param array $configuration
-	 * @return static
+	 * @param Request $request
+	 * @param Response $response
 	 */
-	public static function createServer(array $configuration = array())
+	protected function dispatch(Request $request, Response $response)
 	{
-		$environment = new Environment($configuration);
+		$route = $this->router->match(
+			$resources,
+			$request->getMethod(),
+			$request->getPathInfo()
+		);
 
-		if ($environment->production()) {
-			$cache = new FileCache($environment->cachePath);
+		switch ($route->status) {
+			case Route::NOT_FOUND:
+				$this->injector->inject($this->notFound);
+				break;
 
-		} else {
-			$cache = new NullCache();
+			case Route::METHOD_NOT_ALLOWED:
+				$response->headers->set("Allow", $route->allowed);
+
+				if ($request->getMethod() === "OPTIONS") {
+					$response->setStatusCode(200);
+				} else {
+					$response->setStatusCode(405);
+
+				}
+				break;
+
+			case Route::FOUND:
+				$request->request->add($route->params);
+
+				foreach ($route->handlers as $handler) {
+					if (!$this->injector->inject($handler)) {
+						return;
+					}
+				}
+				break;
 		}
+	}
 
-		$injector   = new Injector($cache);
-		$router     = new Router($cache, new UrlTools());
-		$dispatcher = new Dispatcher($injector, $router);
+	/**
+	 * @param Request $request
+	 * @param Response $response
+	 */
+	protected function tryDispatch(Request $request, Response $response)
+	{
+		try {
+			$this->dispatch($request, $response);
 
-		$dispatcher->notFound(function (Response $resp)
-		{
-			$resp->setStatusCode(404);
-		});
-
-		$dispatcher->error(function (Environment $environment, Exception $exception, Response $resp)
-		{
-			$resp->setStatusCode(503);
-
-			if (!$environment->production()) {
-				$resp->setContent("<pre>$exception</pre>");
-			}
-		});
-
-		$injector->instance("environment", $environment);
-
-		return new static($dispatcher, $injector);
+		} catch (\Exception $exception) {
+			$this->injector->instance("exception", $exception);
+			$this->injector->inject($this->error);
+		}
 	}
 }
